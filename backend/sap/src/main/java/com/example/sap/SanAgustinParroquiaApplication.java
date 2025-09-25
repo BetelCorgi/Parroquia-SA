595f0919cee21eec4487f09ba130dfc91c942812
BetelCorgi <borogeuse@protonmail.com>
2025-09-25 18:44:29 -0500

    Agregamos la carpeta backend

---
+++ backend/sap/src/main/java/com/example/sap/SanAgustinParroquiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanAgustinParroquiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanAgustinParroquiaApplication.class, args);
+	}
+
+}
