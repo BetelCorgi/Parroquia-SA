47829d87253d25320979cda3e38a32eeb8f37ee5
Cristian02-12 <cristianelinsonolanorodriguez@gmail.com>
2025-10-13 19:12:40 -0500

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Login } from './login/login';
+import { adminRoutes } from './admin/admin.routes';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login', component: Login },
+  { path: 'admin', children: adminRoutes },
   { path: '**', redirectTo: '' },
 ];
